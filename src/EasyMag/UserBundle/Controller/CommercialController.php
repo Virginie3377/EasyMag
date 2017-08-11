924cf930082f6bbefda7f2c5a01a062f758ef671
Virginie3377 <virginiecapdevielle77@gmail.com>
2017-08-12 00:36:50 +0200

    Profile commercial

--- src/EasyMag/UserBundle/Controller/CommercialController.php
+++ src/EasyMag/UserBundle/Controller/CommercialController.php
@@ -17,13 +17,24 @@ class CommercialController extends Controller
         ));
     }
 
-    public function menuAction(){
-
+    public function menuAction()
+    {
 
         return $this->render('EasyMagUserBundle:BackOffice/Commercial:main_menu.html.twig', array(
            //....
         ));
     }
 
+    public function profileAction()
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $commercials = $em->getRepository(Commercial::class)->findOneByUser($this->getUser());
+
+
+        return $this->render('EasyMagUserBundle:BackOffice/Commercial:profile.html.twig', array(
+            'commercials' => $commercials,
+        ));
+    }
 
 }
