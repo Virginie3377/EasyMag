c2e8f58c06f25f5da4a21493edbb1db17216a40e
Unknown <contact@virginiecapdevielle.com>
2018-09-03 23:23:15 +0200

    Souscriber form
    modal page accueil

--- src/EasyMag/UserBundle/Controller/FrontOfficeController.php
+++ src/EasyMag/UserBundle/Controller/FrontOfficeController.php
@@ -5,9 +5,18 @@ namespace EasyMag\UserBundle\Controller;
 use EasyMag\UserBundle\Entity\Commercial;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use EasyMag\UserBundle\Entity\Souscriber;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use EasyMag\UserBundle\Form\SouscriberType;
+
 
 class FrontOfficeController extends Controller
 {
+    /**
+     * Lists all customer entities.
+     *
+     */
     public function indexAction(Request $request)
     {
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
@@ -23,6 +32,54 @@ class FrontOfficeController extends Controller
 
 
         }
-            return $this->render('EasyMagUserBundle:FrontOffice:index.html.twig');
+        $souscriber = new Souscriber();
+        $form = $this->createForm(SouscriberType::class, $souscriber);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($souscriber);
+            $em->flush();
+
+        return $this->redirectToRoute('easy_mag_user_homepage');
+        }
+
+
+            return $this->render('EasyMagUserBundle:FrontOffice:index.html.twig', array(
+            'souscriber' => $souscriber,
+            'form' => $form->createView(),
+        ));
+
+    }
+
+    public function contactAction()
+    {
+        $message = \Swift_Message::newInstance()
+                                 ->setSubject('Formulaire de reception')
+                                 ->setFrom('send@example.com')
+                                 ->setTo('contact@lessentieldevotreville.fr')
+                                 ->setBody($this->renderView(
+                                     'EasyMagUserBundle::contact_email.html.twig', array(
+                                     'nom'    => $_POST['nom'],
+                                     'email'   => $_POST['email'],
+                                     'tel'     => $_POST['tel'],
+                                     'message' => $_POST['message']
+                                 )), 'text/html'
+                                 );
+
+        $this->get('mailer')->send($message);
+        $this->addFlash('info', 'Votre message a bien été envoyée.');
+
+        return $this->redirectToRoute('easy_mag_user_homepage');
+    }
+
+    
+   
+
+    public function magEnLigneAction()
+    {
+        return $this->render('EasyMagUserBundle:FrontOffice:mag_en_ligne.html.twig', array(
+            //....
+        ));
     }
 }
---
+++ src/EasyMag/UserBundle/Entity/Souscriber.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace EasyMag\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Souscriber
+ *
+ * @ORM\Table(name="souscriber")
+ * @ORM\Entity(repositoryClass="EasyMag\UserBundle\Repository\SouscriberRepository")
+ */
+class Souscriber
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="zipCode", type="string", length=5)
+     */
+    private $zipCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Souscriber
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return Souscriber
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set zipCode
+     *
+     * @param string $zipCode
+     *
+     * @return Souscriber
+     */
+    public function setZipCode($zipCode)
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    /**
+     * Get zipCode
+     *
+     * @return string
+     */
+    public function getZipCode()
+    {
+        return $this->zipCode;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return Souscriber
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+}
+
